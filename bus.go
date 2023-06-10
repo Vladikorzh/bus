9ee2e0cd6b4672203b20e7eac611e05930ff8b2e
Vladimir <korzhenevskiy@gnivc.ru>
2023-06-10 11:36:45 +0300

    v1.0.1

--- bus.go
+++ bus.go
@@ -11,14 +11,25 @@ var (
 	ErrInvalidMessageType = errors.New("invalid message type")
 )
 
-var instance Bus = New()
-
 type Message any
 
-type Handler func(ctx context.Context, msg Message) error
+type Handler interface {
+	handle(ctx context.Context, msg Message) error
+}
+
+type HandlerFunc[T any] func(ctx context.Context, msg *T) error
+
+func (fn HandlerFunc[T]) handle(ctx context.Context, msg Message) error {
+	switch m := msg.(type) {
+	case *T:
+		return fn(ctx, m)
+	default:
+		return ErrInvalidMessageType
+	}
+}
 
 type Bus interface {
-	Handle(msg any, handler Handler)
+	Handle(handler Handler)
 	Publish(ctx context.Context, msg Message) error
 }
 
@@ -32,36 +43,15 @@ func New() Bus {
 	}
 }
 
-func (b *bus) Handle(msg any, handler Handler) {
-	b.handlers[reflect.TypeOf(msg).Name()] = handler
+func (b *bus) Handle(handler Handler) {
+	b.handlers[reflect.TypeOf(handler).In(1).Elem().Name()] = handler
 }
 
 func (b *bus) Publish(ctx context.Context, msg Message) error {
 	handler, ok := b.handlers[reflect.TypeOf(msg).Elem().Name()]
 	if ok {
-		return handler(ctx, msg)
+		return handler.handle(ctx, msg)
 	}
 
 	return ErrHandlerNotFound
-}
-
-func GenericHandler[T any](handler func(ctx context.Context, msg *T)) Handler {
-	return func(ctx context.Context, msg Message) error {
-		switch m := msg.(type) {
-		case *T:
-			handler(ctx, m)
-		default:
-			return ErrInvalidMessageType
-		}
-
-		return nil
-	}
-}
-
-func Handle[T any](msg T, handler func(ctx context.Context, msg *T)) {
-	instance.Handle(msg, GenericHandler(handler))
-}
-
-func Publish(ctx context.Context, msg Message) error {
-	return instance.Publish(ctx, msg)
 }
